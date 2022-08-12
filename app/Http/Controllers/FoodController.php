573eef56437b76a1c1c561055c3bdde5f13a3dbd
Sensen <senoagung27@gmail.com>
2022-08-12 16:25:10 +0700

    add controller dashboard food transaction user

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRequest;
-use App\Models\User;
+use App\Http\Requests\FoodRequest;
+use App\Models\Food;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
-class UserController extends Controller
+class FoodController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,21 +16,21 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::paginate(10);
+        $food = Food::paginate(10);
 
-        return view('users.index', [
-            'user' => $user
+        return view('food.index', [
+            'food' => $food
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        return view('users.create');
+        return view('food.create');
     }
 
     /**
@@ -38,24 +39,24 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(UserRequest $request)
+    public function store(FoodRequest $request)
     {
         $data = $request->all();
 
-        $data['picturePath'] = $request->file('picturePath')->store('assets/user', 'public');
+        $data['picturePath'] = $request->file('picturePath')->store('assets/food', 'public');
 
-        User::create($data);
+        Food::create($data);
 
-        return redirect()->route('users.index');
+        return redirect()->route('food.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param  \App\Models\Food  $food
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
-    public function show(User $user)
+    public function show(Food $food)
     {
         //
     }
@@ -63,13 +64,13 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param  \App\Models\Food  $food
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
-    public function edit(User $user)
+    public function edit(Food $food)
     {
-        return view('users.edit',[
-            'item' => $user
+        return view('food.edit',[
+            'item' => $food
         ]);
     }
 
@@ -77,33 +78,33 @@ class UserController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
+     * @param  \App\Models\Food  $food
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, Food $food)
     {
         $data = $request->all();
 
         if($request->file('picturePath'))
         {
-            $data['picturePath'] = $request->file('picturePath')->store('assets/user', 'public');
+            $data['picturePath'] = $request->file('picturePath')->store('assets/food', 'public');
         }
 
-        $user->update($data);
+        $food->update($data);
 
-        return redirect()->route('users.index');
+        return redirect()->route('food.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\User  $user
+     * @param  \App\Models\Food  $food
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy(User $user)
+    public function destroy(Food $food)
     {
-        $user->delete();
+        $food->delete();
 
-        return redirect()->route('users.index');
+        return redirect()->route('food.index');
     }
 }
