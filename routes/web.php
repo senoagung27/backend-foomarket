2bd0841981529902dc555eca24e36e17d66936e8
Sensen <senoagung27@gmail.com>
2022-08-12 16:41:26 +0700

    debug route and add view user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\API\MidtransController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +19,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return redirect()->route('admin-dashboard');
+});
+
+// Dashboard
+// Route::prefix('dashboard')
+//     ->middleware(['auth:sanctum','admin'])
+//     ->group(function() {
+//         Route::get('/', [DashboardController::class, 'index'])
+//             ->name('admin-dashboard');
+//         Route::resource('food', FoodController::class);
+//         Route::resource('users', UserController::class);
+
+//         Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])
+//             ->name('transactions.changeStatus');
+//         Route::resource('transactions', TransactionController::class);
+//     });
+
+// // Midtrans Related
+// Route::get('midtrans/success', [MidtransController::class, 'success']);
+// Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
+// Route::get('midtrans/error', [MidtransController::class, 'error']);
+
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group([ "middleware" => ['auth:sanctum', 'verified'] ], function() {
-    Route::view('/dashboard', "dashboard")->name('dashboard');
+Route::group([ "middleware" => ['auth:sanctum', 'admin'] ], function() {
+    // Route::view('/dashboard', "dashboard")->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+                ->name('dashboard');
 
-    Route::get('/user', [ UserController::class, "index_view" ])->name('user');
+    Route::resource('food', FoodController::class);
+        Route::resource('users', UserController::class);
+
+        Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])
+            ->name('transactions.changeStatus');
+        Route::resource('transactions', TransactionController::class);
+
+    Route::get('/user', [ UserController::class, "index" ])->name('user');
     Route::view('/user/new', "pages.user.user-new")->name('user.new');
     Route::view('/user/edit/{userId}', "pages.user.user-edit")->name('user.edit');
 });
+Route::get('midtrans/success', [MidtransController::class, 'success']);
+Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
+Route::get('midtrans/error', [MidtransController::class, 'error']);
